45603b478535fd011b9ab7c67544614ac13fa58d
Mislav Ćavarušić <micavaru@vsite.hr>
2024-12-12 19:00:16 +0100

    Resolved BlazorServer Tutorijal

---
+++ TODO.CoreHosted/Server/Migrations/20241212173552_ToDOInitialMigration.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TODO.CoreHosted.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class ToDOInitialMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
