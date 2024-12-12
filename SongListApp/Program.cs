066a461c5ecae6256401518505db740b9abe0276
Mislav Ćavarušić <micavaru@vsite.hr>
2024-12-12 18:18:08 +0100

    Resolved SongListApp

--- SongListApp/Program.cs
+++ SongListApp/Program.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
 using SongListApp.Data;
+using SongListApp.Services;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +11,9 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") ?? throw new InvalidOperationException("Connection string DefaultConnString not found.")));
+builder.Services.AddScoped<SongService>();
+
 
 var app = builder.Build();
 
