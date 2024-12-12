c84294ca72bee740cf60bc96555bb03f19802de0
Lara Mihelin <lamihelin@vsite.hr>
2024-12-12 18:24:06 +0100

    song list is over

--- SongListApp/Program.cs
+++ SongListApp/Program.cs
@@ -1,13 +1,20 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
+using Microsoft.EntityFrameworkCore;
 using SongListApp.Data;
+using SongListApp.Services;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection") 
+    ?? throw new InvalidOperationException("Connection string DefaultConnStringnot found.")));
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+
+builder.Services.AddScoped<SongService>();
 
 var app = builder.Build();
 
