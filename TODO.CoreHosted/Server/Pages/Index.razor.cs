e7c7a0b3f46abae796996858d7f47483b9a4da8c
Lara Mihelin <lamihelin@vsite.hr>
2024-12-13 18:47:43 +0100

    ToDo is done

---
+++ TODO.CoreHosted/Server/Pages/Index.razor.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using TODO.CoreHosted.Shared;
+
+namespace TODO.CoreHosted.Server.Pages
+{
+	public partial class Index
+	{
+		[Inject] public HttpClient Http { get; set; }
+
+		private IList<TodoItem> tasks;
+		private string error;
+
+		protected override async Task OnInitializedAsync()
+		{
+			try
+			{
+				string requestUri = "TodoItems"; //ovo je naziv kontrolera//pozivamo metodu na kontroleru za dohvat todo-ova
+				tasks = await Http.GetFromJsonAsync<IList<TodoItem>>(requestUri);
+			}
+			catch (Exception)
+			{
+				error = "Error Encountered";
+			}
+		}
+	}
+}
