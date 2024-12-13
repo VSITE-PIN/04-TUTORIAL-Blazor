f7c91e26f84958ca4bbd61450e14257492d0b1d6
Dorijan Ivanković <doivanko@vsite.hr>
2024-12-13 18:39:50 +0100

    rijeseno

--- SongListApp/Program.cs
+++ SongListApp/Program.cs
@@ -1,13 +1,18 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using SongListApp.Data;
+using SongListApp.Services;
+using System;
 
 var builder = WebApplication.CreateBuilder(args);
+
+
 
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddScoped<SongsService>();
 
 var app = builder.Build();
 
